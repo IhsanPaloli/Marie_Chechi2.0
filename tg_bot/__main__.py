3fb2842c755517bb576b9243828b18bde9ce544c
Ihsan <88818968+IhsanPaloli@users.noreply.github.com>
2021-08-26 07:23:49 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -144,10 +144,9 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-              
+             
                 parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="മുതലാളി 💖",url="https://t.me/Ihsan_paloli"),
-                                           
+                                                         
                     
     else:
         update.effective_message.reply_text("ഹായ് മച്ചാൻ,  എന്താണ്??")
